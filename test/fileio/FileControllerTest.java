670f7ecd9cf18103f1a343d16e69a576e13efeef
ito <002@192.168.1.82>
2018-06-21 16:29:14 +0900

    FileControllerTestクラスを追加します（未完）

---
+++ test/fileio/FileControllerTest.java
@@ -0,0 +1,54 @@
+package fileio;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FileControllerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMember() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testAccount() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testPlace() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testDepart() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testOutput() {
+		fail("まだ実装されていません");
+	}
+
+}
