1b4d17a11483721c2c397e2e830f2fbbe417e8d6
nagashima <033@192.168.1.81>
2018-06-21 11:12:15 +0900

    テストコードを作成しました。それに伴うDaoの更新をかけました

---
+++ test/dao/DepartDaoImplTest.java
@@ -0,0 +1,176 @@
+package dao;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.TestDBAccess;
+
+import domain.Depart;
+
+public class DepartDaoImplTest extends TestDBAccess{
+
+	static DataSource  ds;
+	//正常系
+	final String CORRECT="100";
+	final String DEPARTMENT="技術部";
+	final String DEPARTMENT2="企画部";
+	final int FLOOR=2;
+	final int FLOOR2=2;
+	final static int POSITIONTYPE=200;
+	final static int POSITIONTYPE2=201;
+
+	//異常系
+	final String FAIL_DEPARTMENT="技術部";
+	final int FAIL_FLOOR=12;
+	final int FAIL_POSITIONTYPE=792310;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		InitialContext ctx=null;
+
+		try {
+			ctx=new InitialContext();
+			ds=(DataSource)ctx.lookup("java:comp/env/jdbc/eventdb2");
+		} catch (NamingException e) {
+			if (ctx != null) {
+				try {
+					ctx.close();
+				} catch (NamingException el) {
+					throw new RuntimeException(el);
+				}
+			}
+			throw new RuntimeException(e);
+	}
+
+		try (Connection con = ds.getConnection()){
+			PreparedStatement stmt;
+			String sql;
+
+
+			sql="TRUNCATE department";
+			stmt=con.prepareStatement(sql);
+			stmt.executeUpdate();
+
+			//テストデータの登録
+			sql=	"INSERT INTO `members` VALUES ('200','山本葵','ヤマモトアオイ','1995-12-10','東京都新宿区飯田橋54-10-1','090-6433-1233','2018-04-02',1,0,'maxmurai'),"
+					+ "('201','中村悠真','ナカムラユウマ','1995-12-11','東京都新宿区飯田橋54-10-1','090-6433-1234','2018-04-02',1,0,'hikakinn')";
+			stmt=con.prepareStatement(sql);
+			stmt.executeUpdate();
+		}
+
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+
+		try (Connection con = ds.getConnection()){
+			PreparedStatement stmt;
+			String sql;
+
+			sql="DELETE FROM `eventdb2`.`members` WHERE `member_id`="+POSITIONTYPE;
+			stmt=con.prepareStatement(sql);
+			stmt.executeUpdate();
+
+			sql="DELETE FROM `eventdb2`.`members` WHERE `member_id`="+POSITIONTYPE2;
+			stmt=con.prepareStatement(sql);
+			stmt.executeUpdate();
+		}
+
+	}
+//
+//	@Before
+//	public void setUp() throws Exception {
+//	}
+//
+	@After
+	public void tearDown() throws Exception {
+		try (Connection con = ds.getConnection()){
+			PreparedStatement stmt;
+			String sql;
+
+
+			sql="TRUNCATE department";
+			stmt=con.prepareStatement(sql);
+			stmt.executeUpdate();
+		}
+	}
+
+//	@Test
+//	public void testDepartDaoImpl() {
+//
+//	}
+
+	@Test
+	public void testInsert正常() throws Exception {
+		DepartDao departDao=DaoFactory.createDepartDao();
+		Depart dep=new Depart();
+		dep.setDepartment(DEPARTMENT);
+		dep.setFloor(FLOOR);
+		dep.setPosition_type(POSITIONTYPE);
+		Depart dep2=new Depart();
+		dep2.setDepartment(DEPARTMENT2);
+		dep2.setFloor(FLOOR2);
+		dep2.setPosition_type(POSITIONTYPE2);
+		List<Depart> DepList=new ArrayList<>();
+		DepList.add(0, dep);
+		System.out.println(DepList.get(0).getDepartment());
+		DepList.add(1, dep2);
+		//departDao.insert(DepList);
+		String result=departDao.insert(DepList);
+
+		//テーブルを整える
+		assertThat(result,is(CORRECT));
+
+
+	}
+
+	@Test
+	public void testInsert異常1() throws Exception {
+		DepartDao departDao=DaoFactory.createDepartDao();
+		Depart dep=new Depart();
+		dep.setDepartment(DEPARTMENT);
+		dep.setFloor(FLOOR);
+		dep.setPosition_type(POSITIONTYPE);
+		List<Depart> DepList=new ArrayList<>();
+		DepList.add(0, dep);
+		departDao.insert(DepList);
+
+
+		dep.setDepartment(FAIL_DEPARTMENT);
+		dep.setFloor(FAIL_FLOOR);
+		dep.setPosition_type(FAIL_POSITIONTYPE);
+		DepList.add(0, dep);
+
+		String result=departDao.insert(DepList);
+
+		assertThat(result,is("300"));
+	}
+
+	@Test
+	public void testInsert異常2() throws Exception {
+		DepartDao departDao=DaoFactory.createDepartDao();
+		Depart dep=new Depart();
+		List<Depart> DepList=new ArrayList<>();
+		dep.setDepartment(DEPARTMENT2);
+		dep.setFloor(FAIL_FLOOR);
+		dep.setPosition_type(FAIL_POSITIONTYPE);
+		DepList.add(0, dep);
+
+		String result=departDao.insert(DepList);
+		assertThat(result,is("302"));
+	}
+
+}
